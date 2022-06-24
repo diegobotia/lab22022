cca4ba9223e08fe558390df1a0a2de90031681c0
DB <diego.botia@udea.edu.co>
2022-06-23 19:13:01 -0500

    Calculadora

--- app/calculator.js
+++ app/calculator.js
@@ -1,7 +1,14 @@
 'use strict';
 
 function add(a,b){
-    return a+b;
+  let uno=document.getElementById("n1");
+  let dos=document.getElementById("n2");
+  let myTotal= document.getElementById("total");
+  let suma=parseInt(uno.value) + parseInt(dos.value);
+  myTotal.innerText= "El resultado es: " + suma;
+  
+  
+    // return a+b;
 }
 
 function minus(a,b){
